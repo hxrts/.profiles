e3c7a7e3a69da894a5169974bb92dab072a0245c
Samuel Berman <bermans@mskcc-ln1.local>
2016-11-28 17:03:38 -0500

    initi script revision

--- README.md
+++ README.md
@@ -4,24 +4,7 @@ profiles
 for syncing profile settings across machines
 
 ## instructions
-### bash
-~/.bash\_profile should source profiles folder with `source ~/profiles/.bash_profile`
 
-linux boxes can add to ~/.bashrc the following to source gitprofiles folder `source ~/profiles/.bashrc`
+1. make sure rsa keys are setup for github
 
-### vim
-create directory ~/.vim/backup for vim backups
-
-~/.vimrc should source the gitprofiles folder with `:so ~/profiles/.vimrc`
-
-### R
-~/.Rprofile should source the profiles folder with `source("~/profiles/.Rprofile")`
-
-### git
-add the following to ~/.gitconfig to source code in profiles/.gitconfig
-
-```
-[include]
-	path = profiles/.gitconfig
-```
-
+2. from home directory run ~/profiles/init.sh
--- init.sh
+++ init.sh
@@ -1,70 +1,37 @@
-#! /bin/bash
+#-----
+# bash
+#-----
 
-#--------------------
-# add source function
-#--------------------
+echo 'source ~/profiles/.bash_profile' >> ~/.bash_profile
 
-add_source() {
-
-	source_cmd=$1
-	profile=$2
-
-	if [ -e "$profile" ] ; then
-		echo -e "- found" $profile
-		if ! grep -qF "$source_cmd" $profile ; then
-			echo -e "  adding source cmd\n"
-			profile_content=$(cat $profile)
-			echo -en "$source_cmd\n$profile_content" > $profile
-		else
-			echo -e "  source string present\n"
-			return 1
-		fi
-	else
-		echo -e "- building" $profile "\n"
-		echo -e "$source_cmd" > $profile
-	fi
-}
-
-
-#-------
-# .bash*
-#-------
-
-# .bash_profile
-add_source "source $HOME/profiles/.bash_profile" $HOME/.bash_profile
-
-# .bashrc
-add_source "source $HOME/profiles/.bashrc" $HOME/.bashrc
-
-
-#----
-# vim
-#----
-
-# .vimrc
-add_source ":so $HOME/profiles/.vimrc" $HOME/.vimrc
-
-# vim pathogen package manager install & theme
-mkdir -p $HOME/.vim/backup $HOME/.vim/autoload $HOME/.vim/bundle && \
-curl -LSso $HOME/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim && \
-if ! [ -e $HOME/.vim/bundle/vim-colors-solarized ] ; then
-	git clone git://github.com/altercation/vim-colors-solarized.git $HOME/.vim/bundle/vim-colors-solarized
-fi
-
-
-#--
-# R
-#--
-
-# .Rprofile
-add_source "source(\"$HOME/profiles/.Rprofile\")" $HOME/.Rprofile
+echo 'source ~/profiles/.bashrc' >> ~/.bashrc
 
 
 #----
 # git
 #----
 
-# .gitconfig
-add_source "	path = profiles/.gitconfig" $HOME/.gitconfig && \
-add_source '[include]' $HOME/.gitconfig
+printf '[include]\n\tpath = profiles/.gitconfig' >> ~/.gitconfig
+
+
+#----
+# vim
+#----
+
+mkdir -p ~/.vim/backup
+
+echo ':so ~/profiles/.vimrc' >> ~/.vimrc
+
+mkdir -p ~/.vim/autoload ~/.vim/bundle && \
+		curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+
+mkdir -p ~/.vim/bundle
+git clone git://github.com/altercation/vim-colors-solarized.git ~/.vim/bundle/vim-colors-solarized
+
+
+#--
+# R
+#--
+
+echo 'source("~/profiles/.Rprofile")' >> ~/.Rprofile
 
